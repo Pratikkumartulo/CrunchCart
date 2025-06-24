2a32a53a3bdccd9a80d9fb3b7abfe3175a85fe88
Adikumar7 <radityakumardora@gmail.com>
2025-06-24 18:15:09 +0530

    image added successfully

--- chips.py
+++ chips.py
@@ -1,27 +1,73 @@
 chipsData = {
     1: {
-        "name": "Wireless Mouse",
-        "price": 599.00,
+        "name": "Sour Cream & Cheddar",
+        "price": 89.00,
         "image_url": "img1.jpg"
     },
     2: {
-        "name": "Bluetooth Headphones",
-        "price": 1299.00,
-        "image_url": "/static/images/headphones.jpg"
+        "name": "Ginger",
+        "price": 29.00,
+        "image_url": "img2.jpg"
     },
     3: {
-        "name": "Cotton T-Shirt",
-        "price": 499.00,
-        "image_url": "/static/images/tshirt.jpg"
+        "name": "Carolina Reaper",
+        "price": 99.00,
+        "image_url": "img3.jpg"
     },
     4: {
-        "name": "Notebook",
-        "price": 149.00,
-        "image_url": "/static/images/notebook.jpg"
+        "name": "Salt & Vinegar",
+        "price": 59.00,
+        "image_url": "img4.jpg"
     },
     5: {
-        "name": "Sports Water Bottle",
-        "price": 299.00,
-        "image_url": "/static/images/water_bottle.jpg"
-    }
+        "name": "Dill Pickle",
+        "price": 49.00,
+        "image_url": "img5.jpg"
+    },
+    6: {
+        "name": "Sour Cream & Onion",
+        "price": 79.00,
+        "image_url": "img6.jpg"
+    },
+    7: {
+        "name": "Barbecue",
+        "price": 109.00,
+        "image_url": "img7.jpg"
+    },
+    8: {
+        "name": "Chile Limon",
+        "price": 89.00,
+        "image_url": "img8.jpg"
+    },
+    9: {
+        "name": "Garlic",
+        "price": 69.00,
+        "image_url": "img10.jpg"
+    },
+    10: {
+        "name": "Spicy Schezwan",
+        "price": 19.00,
+        "image_url": "img11.jpg"
+    },
+    11: {
+        "name": "Sweet & Spicy Honey",
+        "price": 39.00,
+        "image_url": "img12.jpg"
+    },
+    12: {
+        "name": "Adobadas",
+        "price": 29.00,
+        "image_url": "img13.jpg"
+    },
+    13: {
+        "name": "Spicy Peri Peri",
+        "price": 29.00,
+        "image_url": "img14.jpg"
+    },
+    14: {
+        "name": "Classic(Salted)",
+        "price": 59.00,
+        "image_url": "img15.jpg"
+    },
+    
 }
