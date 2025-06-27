80d8db3fc48de6deb39268219b8afd0affc4dd3c
pratikkumartulo <pratiktulo42@gmail.com>
2025-06-27 15:06:35 +0530

    Added navbar auth and user.html

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, flash, render_template, redirect, url_for,flash
+from flask import Flask, flash, render_template, redirect, url_for,flash,session
 from forms import SignupForm, LoginForm
 from chips import chipsData,findMaxThreeDiscount
-from users import user_data,addUser,validateUser
+from users import user_data,addUser,validateUser,addSession,isLoggedIn,logoutUser
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "15bb914121bfb88e90cd224850b5e614"
@@ -9,26 +9,39 @@ app.config["SECRET_KEY"] = "15bb914121bfb88e90cd224850b5e614"
 @app.route("/")
 @app.route("/home")
 def home():
+    email = isLoggedIn()
     discounted = findMaxThreeDiscount(chipsData)
-    return render_template("home.html",title="Home",chips = discounted)
+    return render_template("home.html",title="Home",chips = discounted,user=email)
 
 @app.route("/about")
 def about():
-    return render_template("about.html",title="About")
+    email = isLoggedIn()
+    return render_template("about.html",title="About",user=email)
 
 @app.route("/contact") 
 def contact():
-    return render_template("contact.html",title="contact")
+    email = isLoggedIn()
+    return render_template("contact.html",title="contact",user=email)
 
 
 @app.route("/favourites") 
 def favourites():
-    return render_template("favourites.html",title="favourites",chipsData = chipsData)
+    email = isLoggedIn()
+    return render_template("favourites.html",title="favourites",chipsData = chipsData,user=email)
 
 
 @app.route("/products")
 def products():
-    return render_template("products.html",title="products",chips = chipsData)
+    email = isLoggedIn()
+    return render_template("products.html",title="products",chips = chipsData,user=email)
+
+@app.route('/user')
+def userProfile():
+    curUser = isLoggedIn()
+    if curUser:
+        return render_template('user.html', title='User',user = curUser)
+    else:
+        return redirect(url_for('login'))
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -36,6 +49,7 @@ def login():
     if form.validate_on_submit():
         if validateUser(form.email.data,form.password.data):
             flash('Login successfull!','success')
+            addSession(form.email.data)
             return redirect(url_for('home'))
         else:
             flash('Invalid email or password','error')
@@ -47,23 +61,30 @@ def signup():
     form = SignupForm()
     if form.validate_on_submit():
         if addUser(form.username.data,form.email.data,form.gender.data,form.dob.data,form.password.data):
+            addSession(form.email.data)
             flash(f'Signup successful! Welcome {form.username.data}!','success')
             return redirect(url_for('home'))
         else:
             flash(f'Email already exists !!','error')
-    else:
-        flash(f'Some error occured !!','error')
 
     return render_template('signup.html', title="Signup", form=form)
 
 @app.route("/product/<id>")
 def ProductDetail(id):
-    print(f"The chip id is {type(id)}")
-    chip = chipsData.get(int(id))
-    print(chip)
-    if not chip:
-        return redirect(url_for('products'))
-    return render_template('product_detail.html',title=f"CrunchCart - {chip['name']}",chipDet = chip)
+    email = isLoggedIn()
+    if email:
+        chip = chipsData.get(int(id))
+        if not chip:
+            return redirect(url_for('products'))
+        return render_template('product_detail.html',title=f"CrunchCart - {chip['name']}",chipDet = chip,user=email)
+    else:
+        flash("You need to login for this action","info")
+        return redirect(url_for('login'))
+
+@app.route("/logout")
+def logout():
+    logoutUser()
+    return redirect(url_for('home'))
 
 
 
