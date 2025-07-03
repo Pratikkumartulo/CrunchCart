084db753552835bd74f0956669d7511ea7825ef8
pratikkumartulo <pratiktulo42@gmail.com>
2025-07-03 16:31:29 +0530

    Modified config file

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-DB_USER = "root"
-DB_PASSWORD = "12345678"
+DB_USER = "<Your DB User>"
+DB_PASSWORD = "<Your DB Password>"
 DB_NAME = "CrunchCart"
 DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@localhost:3306/{DB_NAME}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "15bb914121bfb88e90cd224850b5e614"
+SECRET_KEY = "your_secret_key_here"
 ADMIN_ID = "admin@gmail.com"
 ADMIN_PASS = "admin1234"
