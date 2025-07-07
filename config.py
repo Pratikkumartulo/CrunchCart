f9fe9eb85897da01203c579e554981a3c5a35b72
Adikumar7 <radityakumardora@gmail.com>
2025-07-07 20:21:10 +0530

    first commit

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-DB_USER = ""
-DB_PASSWORD = ""
+DB_USER = "root"
+DB_PASSWORD = "12345678"
 DB_NAME = "CrunchCart"
 DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@localhost:3306/{DB_NAME}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
